c639282dec2b0b88d501fbef9ebdc49645671d8d
jaspreet a singh <jaspreet_a_singh@ultimatesoftware.com>
2022-01-07 18:18:08 +0530

    really

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 console.log("Listening on http://localhost:8000");
 serve((_req) => {
-    return new Response("Hello World new!", {
+    return new Response("Hello World really!", {
         headers: { "content-type": "text/plain" },
     });
 });
